9353e0fc412f706d2158918fb660d593f5bb95e6
Travis CI <bud-team@criteo.com>
2019-06-27 15:27:39 +0000

    Automatic update of SDK - v1.0.14

--- src/main/java/com/criteo/marketing/ApiClient.java
+++ src/main/java/com/criteo/marketing/ApiClient.java
@@ -98,7 +98,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("OpenAPI-Generator/1.0.13/java");
+        setUserAgent("OpenAPI-Generator/1.0.14/java");
 
         authentications = new HashMap<String, Authentication>();
     }
